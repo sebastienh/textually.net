a34891ffdd45281f1c7133c8b2e7691a78a1ce61
Sebastien hamel <sebastien.hamel@gmail.com>
2019-04-23 15:11:50 -0400

    4 - another try at contact form fix

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -98,50 +98,57 @@ const FormButton = styled(Button)`
   color: black;
 `
 
-  /* <form name="contact" method="POST" action="/contact-form-success" netlify>
-      <p>
-          <Label htmlFor="form-name">Name</Label>
-          <Input name="form-name" type="text" required />
-      </p>
-      <p>
-          <Label htmlFor="email">E-Mail</Label>
-          <Input name="email" type="email" required />
-      </p>
-      <p>
-          <Label htmlFor="message">Your Message</Label>
-          <TextArea name="message" required />
-      </p>
-      <p style={{ marginTop: '1rem' }}>
-          <FormButton type="submit" value="Send" id="Form-submit">
-              Send
-          </FormButton>
-      </p>
-  </form> */
-
 export default class ContactForm extends React.Component {
     render() {
         return (
             <Container {...this.props}>
-              <form name="contact" method="POST" data-netlify="true">
-                <p>
-                  <label>Your Name: <input type="text" name="name" /></label>   
-                </p>
-                <p>
-                  <label>Your Email: <input type="email" name="email" /></label>
-                </p>
-                <p>
-                  <label>Your Role: <select name="role[]" multiple>
-                    <option value="leader">Leader</option>
-                    <option value="follower">Follower</option>
-                  </select></label>
-                </p>
-                <p>
-                  <label>Message: <textarea name="message"></textarea></label>
-                </p>
-                <p>
-                  <button type="submit">Send</button>
-                </p>
-              </form>
+
+              <form
+                  name="contact"
+                  method="post"
+                  action="/success"
+                  data-netlify="true"
+                  data-netlify-honeypot="bot-field"
+                >
+                  <input type="hidden" name="bot-field" />
+                  <div>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" id="name" />
+                  </div>
+                  <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name="email" id="email" />
+                  </div>
+                  <div>
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" id="message" rows="6" required />
+                  </div>
+                  <div>
+                    <input type="submit" value="Drop a line" />
+                    <input type="reset" value="Eraser" />
+                  </div>
+                </form>
+
+
+                {/* <form name="contact" method="POST" action="/contact-form-success" netlify>
+                    <p>
+                        <Label htmlFor="form-name">Name</Label>
+                        <Input name="form-name" type="text" required />
+                    </p>
+                    <p>
+                        <Label htmlFor="email">E-Mail</Label>
+                        <Input name="email" type="email" required />
+                    </p>
+                    <p>
+                        <Label htmlFor="message">Your Message</Label>
+                        <TextArea name="message" required />
+                    </p>
+                    <p style={{ marginTop: '1rem' }}>
+                        <FormButton type="submit" value="Send" id="Form-submit">
+                            Send
+                        </FormButton>
+                    </p>
+                </form> */}
             </Container>
         );
     }
