27ef8f0ae2a9431beacf681e32007749b5a3703f
Sebastien hamel <sebastien.hamel@gmail.com>
2019-04-24 10:16:31 -0400

    28 - another try at contact form fix

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,6 +7,105 @@ import PageLocation from "../components/PageLocation"
 import PageTemplate from "../components/PageTemplate"
 import { graphql } from 'gatsby'
 
+
+
+const Container = styled(Flex)`
+  justify-content: center;
+
+  form {
+    p {
+      label,
+      input {
+        display: block;
+      }
+      
+      /* desktop */   
+      @media (min-width: 1200px) {
+        input {
+          min-width: 400px;
+        }
+      }
+
+      /* tablet */   
+      @media (min-width: 800px) and (max-width: 1200px) {
+        input {
+          min-width: 350px;
+        }
+      }
+
+      /* mobile */   
+      @media (max-width: 450px) {
+        input {
+          min-width: 220px;
+        }
+      }
+    }
+  }
+`;
+
+const Label = styled.label`
+  margin: 1rem 0 1rem 0;
+  color: black;
+`; 
+
+const Input = styled.input`
+  height: 2rem;
+  border: none;
+  padding: 0.25rem 1rem;
+  box-shadow: 0 1px 0 black;
+  border-radius: 1px;
+  -webkit-box-shadow: 0 1px 0 black;
+  overflow: auto;
+  font: inherit;
+  -webkit-appearance: none;
+
+  &:-internal-autofill-previewed {
+    background-color: #FFFCF7 !important;
+    background-image: none !important;
+    color: rgb(0, 0, 0) !important;
+  }
+
+  &:-internal-autofill-selected {
+    background-color: #FFFCF7 !important;
+    background-image: none !important;
+    color: rgb(0, 0, 0) !important;
+  }
+
+  &:focus {
+    outline: 0;
+    outline-offset: 0px;
+  }
+`;
+
+const TextArea = styled.textarea`
+  height: 2rem;
+  box-shadow: 0 0px 0;
+
+  /* background-color: #dedede; */
+  padding: 0.25rem 1rem;
+  overflow: auto;
+  font: inherit;
+  padding: 1rem;
+  resize: vertical;
+  min-height: 150px;
+  width: 100%;
+
+  &:focus {
+    outline: 0;
+    outline-offset: 0px;
+  }
+`;
+
+const FormButton = styled(Button)`
+  background-color: white;
+  border-style: solid;
+  border-color: black;
+  border-radius: 0px;
+  border-width: 1px;
+  color: black;
+`
+
+
 export default class Index extends Component {
 
   render() {
@@ -32,7 +131,29 @@ export default class Index extends Component {
                 
               </Text>
             </Flex>
-            <ContactForm mt={60}/>
+            <Container mt={60}>
+                <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+                    <input type="hidden" name="form-name" value="contact" />
+                    <input hidden className="hidden" name="bot-field" />
+                    <p>
+                        <Label htmlFor="name">Name</Label>
+                        <Input name="name" type="text" required />
+                    </p>
+                    <p>
+                        <Label htmlFor="email">E-Mail</Label>
+                        <Input name="email" type="email" required />
+                    </p>
+                    <p>
+                        <Label htmlFor="message">Your Message</Label>
+                        <TextArea name="message" required />
+                    </p>
+                    <p style={{ marginTop: '1rem' }}>
+                        <FormButton type="submit" value="Send" id="Form-submit">
+                            Send
+                        </FormButton>
+                    </p>
+                </form>
+            </Container>
           </PageTemplate>
       </PageLocation>
     )
